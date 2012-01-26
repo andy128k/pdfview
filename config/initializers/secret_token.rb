d67932ab1bc2e20e3dea84e1f39da218fed022e8
andy <andy@sandbox>
2012-01-26 13:25:18 +0200

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pdfview::Application.config.secret_token = '3f18dc39875d44267beef1965a93597070c49038476068a9687c6a127637eeef2dd6707cfb099a63896fcb036658e45c0f708fea1c240c1fc2ea968ff9f6a3ec'
